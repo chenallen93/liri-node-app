243b43717bdf216fb21e7b700ef408fd1dfa72c4
Allen Chen <allenchen@Allens-MacBook-Pro.local>
2017-01-27 17:55:31 -0500

    added files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'JEUexiOHMdQqpV47JCSsVnX1u',
+  consumer_secret: 'hgzr8GdkISVuUD6hAAnrqXwtymvX7l9iYFd7ZRW4yOqaej1m9S',
+  access_token_key: '4218502163-m1mxuMnm373Jv8aq1P5hspTfByN7IHcsqY6dq62',
+  access_token_secret: 'kB43w5WT7z83i9zUFbipc42j4ytIdJqcKNSCk6IcVBo2R',
+}
